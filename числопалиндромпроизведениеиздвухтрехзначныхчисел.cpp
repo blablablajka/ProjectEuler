321701c1f0d6bb34e801e32e46e6775c7b0b9fd5
Zhukov Misha <46239008+Spongebob2007@users.noreply.github.com>
2020-03-31 18:34:19 +0300

    Update числопалиндромпроизведениеиздвухтрехзначныхчисел.cpp

--- числопалиндромпроизведениеиздвухтрехзначныхчисел.cpp
+++ числопалиндромпроизведениеиздвухтрехзначныхчисел.cpp
@@ -6,3 +6,52 @@
 Answer: 906609
 
 */
+
+// Замечание 1: Иногда в зависимости от строения цикла нужно смотреть больше ли нынешнее число числа в цикле
+// Замечание 2: Функция палиндром это одна строка:
+//    if (std::equal(str.cbegin(), str.cbegin() + str.size() / 2, str.crbegin())) {
+//        return 1;
+//    }
+// Замечание 3: Функция палиндром сравнивает перевернутую строку с исходной и если они равны,то строка или число палиндром
+
+
+#include <iostream>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+int isPalindrom(int b)
+{
+    string str = to_string(b);
+
+    if (std::equal(str.cbegin(), str.cbegin() + str.size() / 2, str.crbegin())) {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+
+
+int main()
+{
+    int a = 0;
+    for(int i = 100; i <= 999; i++)
+    {
+        for(int ii = 100; ii <= 999; ii++)
+        {
+            if (isPalindrom((i*ii)) == 1)
+            {
+                if(i*ii > a)
+                {
+                    a = i*ii;
+                }
+            }
+        }
+    }
+    cout<<a;
+}
+
